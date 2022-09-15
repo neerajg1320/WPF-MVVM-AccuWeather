d6341ab58a84476fea5337c9ceb130cd1330fa74
Neeraj Gupta <neeraj76@yahoo.com>
2022-09-15 10:50:39 +0530

    Added Button binding to Command

---
+++ ViewModel/Commands/SearchCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WeatherMvvmApp.ViewModel.Commands
+{
+    public class SearchCommand : ICommand
+    {
+        public WeatherVM VM { get; set; }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public SearchCommand(WeatherVM vm)
+        {
+            VM = vm;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            VM.MakeQuery();
+        }
+    }
+}
---
+++ ViewModel/WeatherVM.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherMvvmApp.Model;
+using WeatherMvvmApp.ViewModel.Commands;
+using WeatherMvvmApp.ViewModel.Helpers;
+
+namespace WeatherMvvmApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set { 
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set { 
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+        public SearchCommand SearchCommand { get; set; }
+
+        public WeatherVM()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new City
+                {
+                    LocalizedName = "New York"
+                };
+
+                CurrentConditions = new CurrentConditions
+                {
+                    WeatherText = "Partly Cloudy",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = 21
+                        }
+                    }
+                };
+            }
+
+            SearchCommand = new SearchCommand(this);
+        }
+
+        public async void MakeQuery()
+        {
+            var cities = await AccuWeatherHelper.GetCities(Query);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
